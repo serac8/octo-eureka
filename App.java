56a7d92e289187cc51b6e7a09f7e4308971ff6cb
serac8 <92148837+serac8@users.noreply.github.com>
2021-11-10 10:19:09 +0100

    Actualización README.md

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Sergio Campo Rodríguez");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("10/11/2021");
+    System.out.println("León");
   }
   
 }
